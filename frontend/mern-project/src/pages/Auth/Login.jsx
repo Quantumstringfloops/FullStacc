95c9ef61036273f572cd8679be544f35db2dd3f8
Neil <neilmathewsuby@gmail.com>
2025-06-04 23:11:22 +0530

    login ui

---
+++ frontend/mern-project/src/pages/Auth/Login.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const Login = () => {
+  return (
+    <div>
+        <div className='lg: w-[70%] h-3/4 md:h-full flex flex-col'>
+            <h1 className=''>Welcome Back</h1>
+            <p className=''>
+                Please enter your details to log in
+            </p>
+        </div>
+    </div>
+  )
+}
+
+export default Login
