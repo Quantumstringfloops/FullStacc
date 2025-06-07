4a392873c436736632056a182a53b8462aef3151
Neil <neilmathewsuby@gmail.com>
2025-06-07 14:37:54 +0530

    login ui design and color

--- frontend/mern-project/src/pages/Auth/Login.jsx
+++ frontend/mern-project/src/pages/Auth/Login.jsx
@@ -4,8 +4,8 @@ const Login = () => {
   return (
     <div>
         <div className='lg: w-[70%] h-3/4 md:h-full flex flex-col'>
-            <h1 className=''>Welcome Back</h1>
-            <p className=''>
+            <h1 className='text-xl font-semibold text-black'>Welcome Back</h1>
+            <p className='text-xs text-state-700 mt-[5px] mb-6'>
                 Please enter your details to log in
             </p>
         </div>
