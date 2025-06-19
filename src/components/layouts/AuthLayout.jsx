7fbadfe2771e153391e46248195d31824f5eb175
Neil <neilmathewsuby@gmail.com>
2025-06-19 18:33:00 +0530

    blah

--- src/components/layouts/AuthLayout.jsx
+++ src/components/layouts/AuthLayout.jsx
@@ -1,9 +1,12 @@
 import React from 'react'
 
-const AuthLayout = () => {
-  return (
-    <div>AuthLayout</div>
-  )
-}
+const AuthLayout = ({ children }) => {
+  return <div>
+        <div className='w-screen h-screen md:w-[60vw]'>
+            <h2 className=''>Expense Tracker</h2>
+        </div>
+    </div>
+  
+};
 
 export default AuthLayout
