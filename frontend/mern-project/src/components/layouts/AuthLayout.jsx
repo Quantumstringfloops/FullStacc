b9a65bd595d1bc62f5a06f3814688828e38f0fda
Neil <neilmathewsuby@gmail.com>
2025-06-19 18:26:24 +0530

    login auth

---
+++ frontend/mern-project/src/components/layouts/AuthLayout.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const AuthLayout = ({ children }) => {
+  return <div>
+        <div className='w-screen h-screen md:w-[60vw]'>
+            <h2 className=''>Expense Tracker</h2>
+        </div>
+    </div>
+  
+};
+
+export default AuthLayout
